32ddd3a54148568b68d6c77ed7e61df7750c55cc
andrey <meshcheryakov-a@mail.ua>
2020-01-02 15:46:49 +0200

    fix spacings

--- stm32f103c8t6/gpio_exti/src/main.c
+++ stm32f103c8t6/gpio_exti/src/main.c
@@ -2,8 +2,8 @@
   ******************************************************************************
   * @file    gpio_exti/src/main.c 
   * @author  Meshcheryakov Andrey
-  * @version V0.0.1
-  * @date    24-September-2019
+  * @version V0.0.3
+  * @date    02-January-2020
   * @brief   This code example shows how to use EXTI.
   *
  ===============================================================================
@@ -12,7 +12,7 @@
    - RCC
    - GPIO:
           PA0  - EXTI
-   		    PC13 - LED
+               PC13 - LED
    
  ===============================================================================
                     ##### How to use this example #####
@@ -56,13 +56,13 @@
 void 
 configure_gpio(void)
 {
-	/* Enable power for GPIO PORT C */
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+    /* Enable power for GPIO PORT C */
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
 
-	/* Clear PA1 and PA2 control register bits */
-	GPIOC->CRH &= ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13);
+    /* Clear PA1 and PA2 control register bits */
+    GPIOC->CRH &= ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13);
 
-	/* Configure PA1 and PA2 as Push Pull output at max 10Mhz */
+    /* Configure PA1 and PA2 as Push Pull output at max 10Mhz */
     GPIOC->CRH |= GPIO_CRH_MODE13_0;
 }
 
@@ -70,30 +70,30 @@ configure_gpio(void)
 void
 configure_externalIT(void)
 {
-  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
 
-  EXTI->IMR = 0x0001;
-  EXTI->RTSR = 0x0001;
-  EXTI->FTSR = 0x0001;
+    EXTI->IMR = 0x0001;
+    EXTI->RTSR = 0x0001;
+    EXTI->FTSR = 0x0001;
 
-  NVIC_EnableIRQ(EXTI0_IRQn);
-  NVIC_SetPriority(EXTI0_IRQn, 0);     
+    NVIC_EnableIRQ(EXTI0_IRQn);
+    NVIC_SetPriority(EXTI0_IRQn, 0);
 }
 
 void EXTI0_IRQHandler(void)
 {
-  if ((EXTI->PR & 0x0001) != 0)  /* Check line 0 has triggered the IT */
-  {
-    EXTI->PR |= 0x0001; /* Clear the pending bit */
-    GPIOC->ODR ^= (1 << 13); /* Toggle green led on PC13 */
-  }
+    if ((EXTI->PR & 0x0001) != 0)  /* Check line 0 has triggered the IT */
+    {
+        EXTI->PR |= 0x0001;        /* Clear the pending bit */
+        GPIOC->ODR ^= (1 << 13);   /* Toggle green led on PC13 */
+    }
 }
 
 /**
   * @brief  This function :
              - Makes simple delay.
-			This function is only for educational purposes.
-			Do not use it another time. 
+            This function is only for educational purposes.
+            Do not use it another time. 
   * @param  uint32_t delay time
   * @retval None
   */
@@ -101,10 +101,10 @@ void
 delay(uint32_t time)
 {
     while(time--)
-	    __asm 		// inline asm - syntax for gcc with c99
-	    (
-	  	    "nop"				  
-	    );
+        __asm         // inline asm - syntax for gcc with c99
+        (
+              "nop"                  
+        );
 }
 
 /**
@@ -115,15 +115,15 @@ delay(uint32_t time)
 int 
 main(void)
 {
-	configure_gpio();
-  configure_externalIT();
+    configure_gpio();
+    configure_externalIT();
 
-  GPIOC->ODR |= (1 << 13); 
+    GPIOC->ODR |= (1 << 13); 
 
-	/* Infinite loop */
-	while(1)
-	{
-	}
+    /* Infinite loop */
+    while(1)
+    {
+    }
 }
 
 /****************************** END OF FILE ***********************************/
