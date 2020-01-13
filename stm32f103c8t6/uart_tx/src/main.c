b57337c914012a5a9e68d3fcc891c43f3ad8e963
MeshAndrey <meshcheryakov-a@mail.ua>
2020-01-13 10:09:02 +0200

    fix spacings

--- stm32f103c8t6/uart_tx/src/main.c
+++ stm32f103c8t6/uart_tx/src/main.c
@@ -13,7 +13,7 @@
    - SysTick
    - USART1
    - GPIO:
-               PC13 - LED
+            PC13 - LED
             PA9  - MCU's UART Tx
             PA10 - MCU's UART Rx - Not used in this example
 
@@ -67,7 +67,7 @@
 /* Private variables ---------------------------------------------------------*/
 volatile uint32_t counter = 0;
 volatile uint32_t delay_time = 0;
-              char text[TEXT_BUF];
+             char text[TEXT_BUF];
 
 /* End of private variables --------------------------------------------------*/
 
@@ -88,8 +88,8 @@ init_SysTick(void)
 {
     SysTick->LOAD = TimerTick;
     SysTick->VAL  = TimerTick;
-    SysTick->CTRL =    SysTick_CTRL_CLKSOURCE_Msk | 
-                      SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
+    SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | 
+                    SysTick_CTRL_TICKINT_Msk   | SysTick_CTRL_ENABLE_Msk;
 }
 
 /**
@@ -103,8 +103,8 @@ init_gpio_led(void)
 {
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
 
-    GPIOC->CRH &= ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13);
-    GPIOC->CRH |= GPIO_CRH_MODE13_0;
+    GPIOC->CRH   &= ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13);
+    GPIOC->CRH   |= GPIO_CRH_MODE13_0;
 }
 
 /**
@@ -118,13 +118,13 @@ init_gpio_uart(void)
 {
     RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN;     // GPIOA Clock ON. Alter function clock ON
 
-    GPIOA->CRH     &= ~GPIO_CRH_CNF9;                             // Clear CNF bit 9
-    GPIOA->CRH     |= GPIO_CRH_CNF9_1;                            // Set CNF bit 9 to 10 - AFIO Push-Pull
-    GPIOA->CRH     |= GPIO_CRH_MODE9_0;                            // Set MODE bit 9 to Mode 01 = 10MHz
+    GPIOA->CRH   &= ~GPIO_CRH_CNF9;                              // Clear CNF bit 9
+    GPIOA->CRH   |=  GPIO_CRH_CNF9_1;                            // Set CNF bit 9 to 10 - AFIO Push-Pull
+    GPIOA->CRH   |=  GPIO_CRH_MODE9_0;                           // Set MODE bit 9 to Mode 01 = 10MHz
 
-    GPIOA->CRH     &= ~GPIO_CRH_CNF10;                            // Clear CNF bit 9
-    GPIOA->CRH     |= GPIO_CRH_CNF10_0;                            // Set CNF bit 9 to 01 = HiZ
-    GPIOA->CRH     &= ~GPIO_CRH_MODE10;                            // Set MODE bit 9 to Mode 01 = 10MHz
+    GPIOA->CRH   &= ~GPIO_CRH_CNF10;                             // Clear CNF bit 9
+    GPIOA->CRH   |=  GPIO_CRH_CNF10_0;                           // Set CNF bit 9 to 01 = HiZ
+    GPIOA->CRH   &= ~GPIO_CRH_MODE10;                            // Set MODE bit 9 to Mode 01 = 10MHz
 }
 
 /**
@@ -136,9 +136,9 @@ init_gpio_uart(void)
 static void
 init_uart(void)
 {
-    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;                        // USART1 Clock ON
+    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;                         // USART1 Clock ON
     USART1->BRR   = 0x341;                                        // Baudrate for 9600 on 8Mhz
-    USART1->CR1  |= USART_CR1_UE | USART_CR1_TE | USART_CR1_RE;    // USART1 ON, TX ON, RX ON                        
+    USART1->CR1  |= USART_CR1_UE | USART_CR1_TE | USART_CR1_RE;   // USART1 ON, TX ON, RX ON                        
 }
 
 /**
@@ -152,7 +152,7 @@ send_byte(uint8_t data)
 {
     while(!(USART1->SR & USART_SR_TC)); // waiting for bit TC in register SR is 1
 
-    USART1->DR = data;                     // send byte throught uart
+    USART1->DR = data;                  // send byte throught uart
 }
 
 /**
@@ -184,10 +184,10 @@ main(void)
     init_uart();
 
     while(1)
-    {                
+    {
         if (delay_time == 0)
         {
-            sprintf(text, "%d\r\n", (int)counter);            
+            sprintf(text, "%d\r\n", (int)counter);
             send_text(text);
             counter++;
 
